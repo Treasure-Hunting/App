1e0c012984a5ad0030fcc347f245f69f76d2ead0
Naoki Ikeguchi <siketyan@sikeserver.com>
2019-11-01 23:12:08 +0900

    Use player.difficulty.pk as difficulty_pk instead of player.difficulty

--- app/views.py
+++ app/views.py
@@ -89,7 +89,7 @@ class Answer(TemplateView):
         player = get_player(request)
         if (player.progress != kwargs['hint_index']):
             return redirect('app:progress-error')
-        kwargs['difficulty_pk'] = player.difficulty
+        kwargs['difficulty_pk'] = player.difficulty.pk
         return super().get(request, *args, **kwargs)
 
     def post(self, request, *args, **kwargs):
