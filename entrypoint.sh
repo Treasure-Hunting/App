cd16ceb0843d1c44fccc667ad7f51eb21d31ba67
Naoki Ikeguchi <siketyan@sikeserver.com>
2019-11-01 17:45:54 +0900

    Add Dockerfile

---
+++ entrypoint.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+echo "Waiting for PostgreSQL"
+while ! pg_isready -h $DATABASE_HOST -p $DATABASE_PORT -U $DATABASE_USER -d $DATABASE_NAME
+do
+  sleep 1
+done
+
+python manage.py migrate
+python manage.py loaddata initial_data
+python manage.py runserver $HTTP_HOST:$HTTP_PORT
