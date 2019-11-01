b66b27e48f13ff1370dc97d09ef64050fdf4637b
Naoki Ikeguchi <siketyan@sikeserver.com>
2019-11-02 01:53:42 +0900

    Fix default settings module

--- treasure_hunting/wsgi.py
+++ treasure_hunting/wsgi.py
@@ -12,6 +12,6 @@ import os
 from django.core.wsgi import get_wsgi_application
 from django.contrib.staticfiles.handlers import StaticFilesHandler
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'treasure_hunting.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'treasure_hunting.settings.development')
 
 application = StaticFilesHandler(get_wsgi_application())
